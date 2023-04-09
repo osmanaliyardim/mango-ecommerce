24c32c2f3ae84f2932a51be33d6dfa11ecbcd77e
Osman Ali Yardım <osmanaliyardim@gmail.com>
2023-04-09 19:11:19 +0300

    FEAT: Product API
    SQL Server DB created and migrations completed. Product entity and dto created. Repository has implemented with AutoMapper setup.

---
+++ Mango.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Mango.Services.ProductAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.ProductAPI.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
