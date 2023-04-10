39ce3c4cea502b16633328625b903514cd0e297d
Osman Ali Yardım <osmanaliyardim@gmail.com>
2023-04-10 13:13:55 +0300

    FEAT: Base Service for API Requests
    Base service created for API requests to centralize and applied for products.

---
+++ Mango.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using Mango.Web.Models;
+
+namespace Mango.Web.Services.IServices
+{
+    public interface IProductService : IBaseService
+    {
+        Task<T> GetAllProductsAsync<T>();
+
+        Task<T> GetAllProductByIdAsync<T>(int productId);
+
+        Task<T> CreateProductAsync<T>(ProductDto productDto);
+
+        Task<T> UpdateProductAsync<T>(ProductDto productDto);
+
+        Task<T> DeleteProductAsync<T>(int productId);
+    }
+}
